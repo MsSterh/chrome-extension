038dcab8bf69cd8af03efef4401b1bb12876d389
Anastasiia Bakuta <ms.sterh@gmail.com>
2020-01-31 11:01:04 +0200

    Add react to index.html

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,25 @@
-console.log('Привет, Мир!')
+'use strict';
+
+const e = React.createElement;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(e(App), domContainer);
